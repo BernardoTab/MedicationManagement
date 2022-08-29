b68d90f69f7d7559ca60981b63024280b6998b80
Bagga <bernardotab@gmail.com>
2022-08-29 16:30:12 +0100

    Feat: small test

--- MedicationManagement/Program.cs
+++ MedicationManagement/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 Console.WriteLine("Test pushs");
+Console.WriteLine("Test two");
 
 var app = builder.Build();
 
